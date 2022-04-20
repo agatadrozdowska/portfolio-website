b7c72050e69ac4b54c041f2883d87929c3e36f0c
agatadrozdowska <agata.drozdowska@icloud.com>
2022-04-20 18:19:46 -0500

    Adding Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import './header.css';
+import CTA from './CTA';
+import HeaderSocials from './HeaderSocials';
+import Agata from '../../assets/agata.jpeg';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+            <div className='container header__container'>
+                <h5>Hello, I am</h5>
+                <h1>Agata Drozdowska</h1>
+                <h5 className='text-light'>Fullstack Developer</h5>
+                <CTA />
+                <HeaderSocials />
+                <div className='me'>
+                    <img src={Agata} alt="agata" />
+                </div>
+                <a href="#contact" className='scroll__down'>Scroll Down</a>
+            </div>
+
+        </header>
     )
 }
 
