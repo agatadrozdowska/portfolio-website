5db88d879f71bbf0c39135b7e2adf7cbb6ffa1e6
agatadrozdowska <agata.drozdowska@icloud.com>
2022-04-19 19:14:08 -0500

    Adding general styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Experience from './components/experience/Experience';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import Portfolio from './components/portfolio/Portfolio';
+import Testimonials from './components/testimonials/Testimonials';
+
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <Header />
+            <Nav />
+            <About />
+            <Experience />
+            <Portfolio />
+            <Testimonials />
+            <Contact />
+            <Footer />
+
+        </div>
     )
 }
 
