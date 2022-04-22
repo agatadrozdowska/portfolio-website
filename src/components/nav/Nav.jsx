0090c6bb74f48c85c1386050fedcf711efc7972e
agatadrozdowska <agata.drozdowska@icloud.com>
2022-04-22 18:40:25 -0500

    Adding styles to Header and Nav Components

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import './nav.css';
+import { AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineUser } from 'react-icons/ai';
+import { BiBookBookmark } from 'react-icons/bi';
+import { BiMessageSquareAdd } from 'react-icons/bi';
+import { useState } from 'react';
+
+
+
+
 
 const Nav = () => {
+    const [activeNav, setActiveNav] = useState('#');
     return (
-        <div>Nav</div>
+        <nav>
+            <a href="#" className={activeNav === '#' ? 'active' : ''} onClick={() => setActiveNav('#')}><AiOutlineHome /></a>
+            <a href="#about" className={activeNav === '#about' ? 'active' : ''} onClick={() => setActiveNav('#about')}><AiOutlineUser /></a>
+            <a href="#experience" className={activeNav === '#experience' ? 'active' : ''} onClick={() => setActiveNav('#experience')}><BiBookBookmark /></a>
+            <a href="#contact" className={activeNav === '#contact' ? 'active' : ''} onClick={() => setActiveNav('#contact')}><BiMessageSquareAdd /></a>
+        </nav>
     )
 }
 
